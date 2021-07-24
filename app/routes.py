6de13bb662eb20c2f1060890fcda16f88a3f1dcd
Oleksii Smiichuk <smiychuk@gmail.com>
2021-07-24 18:19:59 +0300

    Add index+base templates and route

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+
+# @app.before_request
+# def before_request():
+#     if current_user.is_authenticated:
+#         current_user.last_seen = datetime.utcnow()
+#         db.session.commit()
+
+
+@app.route("/")
+@app.route("/index")
+#@login_required
+def index():
+    user = {'username': 'Oleksii'}
+    return render_template("index.html", title="Kv-DevOps-094", user=user)
+ 
