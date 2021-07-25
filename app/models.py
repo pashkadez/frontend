962252bfa58aa3a9e5496f3ae794d5c5303ca945
Oleksii Smiichuk <smiychuk@gmail.com>
2021-07-25 19:43:30 +0300

    Add Issue table into model

--- app/models.py
+++ app/models.py
@@ -20,6 +20,19 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
+class Issue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    issue_id = db.Column(db.Integer)
+    avatar_url = db.Column(db.String(220))
+    user_id = db.Column(db.String(60))
+    issue_action = db.Column(db.String(60))
+    issue_title = db.Column(db.String(120))
+    issue_state = db.Column(db.String(60))
+    issue_html_url = db.Column(db.String(220))
+    modified_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_html_url = db.Column(db.String(220))
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
