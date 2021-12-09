bff4108c634851be05908c679bfc215fdd5909f6
Daniel Fedorov <df3d0r0v@protonmail.com>
2021-12-10 00:18:29 +0200

    change titles and header to DevOps-Kv-116

--- app/routes.py
+++ app/routes.py
@@ -10,21 +10,21 @@ PORT = os.environ.get('RESTAPI_PORT')
 
 @app.route('/', methods=['GET'])
 def Home():
-    return render_template('index.html', title='Kv-DevOps-094')
+    return render_template('index.html', title='DevOps-Kv-116')
 
 
 @app.route('/issues/', methods=['GET'])
 def getIssues():
     request = requests.get(f'http://{HOST}:{PORT}/issues/')
     request_data = json.loads(request.content)
-    return render_template('issues.html', title='Kv-DevOps-094', request_data=request_data)
+    return render_template('issues.html', title='DevOps-Kv-116', request_data=request_data)
 
 
 @app.route('/labels/', methods=['GET'])
 def getLabels():
     request = requests.get(f'http://{HOST}:{PORT}/labels/')
     request_data = json.loads(request.content)
-    return render_template('labels.html', title='Kv-DevOps-094', request_data=request_data)
+    return render_template('labels.html', title='DevOps-Kv-116', request_data=request_data)
 
 
 @app.route('/issues/by-label/<label>', methods=['GET'])
